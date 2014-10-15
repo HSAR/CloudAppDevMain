1d2e9dcc357e807f0e475772a6b2767f0dc58fea
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-15 19:28:58 +0100

    Test commit, Travis does not appear to be working

--- test/test.py
+++ test/test.py
@@ -3,6 +3,7 @@ from main import application
 
 app = TestApp(application)
 
+# This is a test of the hello world default application
 def test_index():
     response = app.get('/')
     assert 'Hello world!' in str(response)
