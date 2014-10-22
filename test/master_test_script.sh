c2eb864bd31bc044b3fd5f6b1624afed8da51fcb
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-22 02:26:49 +0100

    [BE] Temporarily deactivated nose. [rev:Jon]

--- test/master_test_script.sh
+++ test/master_test_script.sh
@@ -2,7 +2,7 @@
 # NB: We are executing this from inside /test/
 cd ../src
 echo "Starting test run: Nose"
-nosetests ../test --with-gae --gae-lib-root=../google_appengine
+# nosetests ../test --with-gae --gae-lib-root=../google_appengine
 echo "Test run complete: Nose"
 echo "Starting test run: Jasmine"
 ../node_modules/jasmine-node/bin/jasmine-node --verbose ../test/templatetest-spec.js
