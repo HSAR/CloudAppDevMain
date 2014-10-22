0e06121ab773517e6fa78e8a06a616a22e435940
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-22 01:04:31 +0100

    [BE] Example template test now exits before completing [rev:Jon]

--- test/templatetest-spec.js
+++ test/templatetest-spec.js
@@ -8,8 +8,13 @@ describe('basic test', function () {
 
  it('should be on correct page', function (done) {
  
-driver.get('http://localhost:8080/template').then(function() {
-driver.findElement(webdriver.By.tagName("p")).getText().then(function(value) {expect(value).toBe("Hello Generic User"); done();})
-});
-});
+    driver.get('http://localhost:8080/template').then(function() {
+            driver.findElement(webdriver.By.tagName("p")).getText().then(function(value) {
+                    expect(value).toBe("Hello Generic User");
+                    driver.quit();
+                    done();
+                }
+            )
+        });
+    });
 });
