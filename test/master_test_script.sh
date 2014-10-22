f8a6a7ff97d2eb1eb2b0369705e4b5babe91792b
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-22 01:55:36 +0100

    [BE] Remove -e flag from script execution. [rev:Jon]

--- test/master_test_script.sh
+++ test/master_test_script.sh
@@ -1,5 +1,4 @@
 # NB: We are executing this from inside /test/
-set -e
 cd ../src
 nosetests ../test --with-gae --gae-lib-root=../google_appengine
 jasmine-node --verbose ../test/templatetest-spec.js
