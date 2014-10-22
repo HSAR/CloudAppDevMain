26c3c837afa75278b405825b334548d11a4ed5ad
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-22 03:02:46 +0100

    [BE] Disable failing tests [rev:Jon]

--- test/master_test_script.sh
+++ test/master_test_script.sh
@@ -6,11 +6,11 @@ echo "Starting test run: Nose"
 nosetests ../test --with-gae --gae-lib-root=../google_appengine
 echo "Test run complete: Nose"
 echo "----------------------------------------------------------------------"
-echo "Setting up test run: Jasmine"
-../google_appengine/dev_appserver.py ../src &
-sleep 30
-echo "Starting test run: Jasmine"
-jasmine-node --verbose --captureExceptions ../test/templatetest-spec.js
-echo "Test run complete: Jasmine"
-echo "----------------------------------------------------------------------"
+# echo "Setting up test run: Jasmine"
+# ../google_appengine/dev_appserver.py ../src &
+# sleep 30
+# echo "Starting test run: Jasmine"
+# jasmine-node --verbose --captureExceptions ../test/templatetest-spec.js
+# echo "Test run complete: Jasmine"
+# echo "----------------------------------------------------------------------"
 echo "All tests complete."
