ad8eddebab8203663430752fd907d34c910aed58
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-22 01:57:57 +0100

    [BE] Jasmine doesn't appear to be running. Version check? [rev:Jon]

--- test/master_test_script.sh
+++ test/master_test_script.sh
@@ -1,4 +1,6 @@
+#!/bin/sh
 # NB: We are executing this from inside /test/
 cd ../src
 nosetests ../test --with-gae --gae-lib-root=../google_appengine
+echo `jasmine-node --version`
 jasmine-node --verbose ../test/templatetest-spec.js
