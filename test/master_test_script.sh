0181e40ce7efda15454da31cf01d5e706c6e9638
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-22 02:21:09 +0100

    [BE] We must go deeper. [rev:Jon]

--- test/master_test_script.sh
+++ test/master_test_script.sh
@@ -5,7 +5,7 @@ echo "Starting test run: Nose"
 nosetests ../test --with-gae --gae-lib-root=../google_appengine
 echo "Test run complete: Nose"
 echo "Starting test run: Jasmine"
-../node-modules/jasmine-node/bin/jasmine-node --verbose ../test/templatetest-spec.js
+../node_modules/jasmine-node/bin/jasmine-node --verbose ../test/templatetest-spec.js
 echo $?
 echo "Test run complete: Jasmine"
 echo "All tests complete."
