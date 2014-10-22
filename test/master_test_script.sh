5e25530280065e8d180515005af0edb915ad658b
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-22 02:35:10 +0100

    [BE] Added new Jasmine flag --captureExceptions [rev:Jon]

--- test/master_test_script.sh
+++ test/master_test_script.sh
@@ -2,10 +2,10 @@
 # NB: We are executing this from inside /test/
 cd ../src
 echo "Starting test run: Nose"
-# nosetests ../test --with-gae --gae-lib-root=../google_appengine
+nosetests ../test --with-gae --gae-lib-root=../google_appengine
 echo "Test run complete: Nose"
 echo "Starting test run: Jasmine"
-../node_modules/jasmine-node/bin/jasmine-node --verbose ../test/templatetest-spec.js
+jasmine-node --verbose --captureExceptions ../test/templatetest-spec.js
 echo $?
 echo "Test run complete: Jasmine"
 echo "All tests complete."
