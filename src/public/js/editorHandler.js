330399814b0a6dd768f7256c8305b96c02f7555f
Mark Mitchell <mjm2g12@soton.ac.uk>
2014-10-23 15:11:16 +0100

    [FE]more work on editor prototype[FE]

--- src/public/js/editorHandler.js
+++ src/public/js/editorHandler.js
@@ -3,6 +3,10 @@ Handler script for the music editor. Still in early stages
 */
 var barHTML = '<div class="bar">' + '<div class="pitch"></div>'.repeat(12) + '</div>';
 
+var noteValues = {
+	quaver : 1,
+	crotchet : 2
+}
 
 function loadPalette() {
 	//we need to load the pallete items and then make em draggable etc.
@@ -21,8 +25,20 @@ function loadCanvas() {
 	}
 	
 	$('.canvas').droppable({
-
+		over : function(event,ui) {
+			var note;
+			if(ui.draggable.hasClass('note-crotchet')) {
+				note = noteValues.crotchet;
+			} else if(ui.draggable.hasClass('note-quaver')) {
+				note = noteValues.quaver;
+			}
+			drawPreview($(this),ui,note);
+		}
 	});
+}
+
+function drawPreview($target,ui,noteLength) {
+	
 }
 
 
