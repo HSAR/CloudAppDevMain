e4580e52779940768cb1b5f503b00f7f36635ea0
Mark Mitchell <mjm2g12@soton.ac.uk>
2014-10-23 16:43:23 +0100

    [FE] work on ui positioning in editor

--- src/public/js/editorHandler.js
+++ src/public/js/editorHandler.js
@@ -1,6 +1,13 @@
 /*
 Handler script for the music editor. Still in early stages
 */
+String.prototype.repeat = function( num )
+{
+    return new Array( num + 1 ).join( this );
+}
+
+
+
 var barHTML = '<div class="bar">' + '<div class="pitch"></div>'.repeat(12) + '</div>';
 
 var noteValues = {
@@ -8,12 +15,24 @@ var noteValues = {
 	crotchet : 2
 }
 
+var barLength = 8;
+
 function loadPalette() {
 	//we need to load the pallete items and then make em draggable etc.
 	$('.editor .note').draggable({
 		helper : 'clone',
 		appendTo : 'body',
-		revert : 'invalid'
+		revert : 'invalid',
+		drag : function(event,ui) {
+			//ok so we want to see if a preview div has been drawn, and if it has we will update it
+			if($('.preview').length) {
+				var noteValue = getNoteValue($(this));
+				if(noteValue) {
+					drawPreview(ui.offset,noteValue);
+				}
+				
+			}
+		}
 	}); //could be a dangerous game as moved out of pallete
 	//console.log($('.pallete'))
 	console.log('test');
@@ -24,7 +43,7 @@ function loadCanvas() {
 		$('.canvas').append(barHTML);
 	}
 	
-	$('.canvas').droppable({
+	$('.pitch').droppable({
 		over : function(event,ui) {
 			var note;
 			if(ui.draggable.hasClass('note-crotchet')) {
@@ -32,13 +51,25 @@ function loadCanvas() {
 			} else if(ui.draggable.hasClass('note-quaver')) {
 				note = noteValues.quaver;
 			}
-			drawPreview($(this),ui,note);
+			//rawPreview($(this),ui,note);
+			console.log($(this).offset());
+			console.log(event.pageX + "   " + event.pageY);
 		}
 	});
 }
 
-function drawPreview($target,ui,noteLength) {
-	
+function drawPreview(offset,noteLength) {
+	//ok so we can use event.pageX - ui.offset and ignore ones where we get minus values as these are false events
+}
+
+function getNoteValue($target) {
+	var note;
+	if($target.hasClass('note-crotchet')) {
+		note = noteValues.crotchet;
+	} else if($target.hasClass('note-quaver')) {
+		note = noteValues.quaver;
+	}
+	return note;
 }
 
 
