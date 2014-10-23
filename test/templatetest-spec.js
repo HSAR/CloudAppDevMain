5134f1c007ba1dc1a6336f8daa92bac6fae355d4
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-23 10:34:19 +0100

    [FE] Heavily commented the example test for demo purposes. [rev:none]

--- test/templatetest-spec.js
+++ test/templatetest-spec.js
@@ -1,20 +1,30 @@
 var webdriver = require('selenium-webdriver');
- 
+
 var driver = new webdriver.Builder().
-   withCapabilities(webdriver.Capabilities.firefox()).
-   build();
- 
+    withCapabilities(webdriver.Capabilities.firefox()).
+    build();
+
+// This is a test suite, which groups together a bunch of tests.
 describe('basic test', function () {
 
- it('should be on correct page', function (done) {
- 
-    driver.get('http://localhost:8080/template').then(function() {
-        driver.findElement(webdriver.By.tagName("p")).getText().then(function(value) {
-                    expect(value).toBe("Hello Generic User");
-                    driver.quit();
-                    done();
-                }
-            )
+    // This is a test, a single self-contained unit that succeeds or fails. Ignored tests are xit('should be...')
+    it('should be on correct page', function (done) {
+
+        // Fetch page and execute callback when promise complete
+        driver.get('http://localhost:8080/template').then(function () {
+
+            // Find an element on the page and retrieve text within tag - callback, again
+            driver.findElement(webdriver.By.tagName("p")).getText().then(function (value) {
+
+                // Jasmine expect statement. Various other expectations are available.
+                expect(value).toBe("Hello Generic User");
+
+                // Close window - this doesn't matter on the VM, but is mostly for when you run manually
+                driver.quit();
+
+                // Complete test
+                done();
+            });
         });
     });
 });
