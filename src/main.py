b35c142e87a599197b1b95dc7e3a3b3d531f1419
Shu Sam Chen <research.hyperspace@gmail.com>
2014-10-30 17:09:02 +0000

    [BE] Overhaul of indentation in main server class. [rev:none]

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,35 +26,40 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello world!')
 
+
 class TemplatePageHandler(webapp2.RequestHandler):
     def get(self):
-	template_values = {
-		'name': "Generic User",
+        template_values = {
+            'name': "Generic User",
         }
         template = JINJA_ENVIRONMENT.get_template('templates/index.html')
         self.response.write(template.render(template_values))
 
+
 class EditorPageHandler(webapp2.RequestHandler):
-	def get(self):
-		template = JINJA_ENVIRONMENT.get_template('templates/editor.html')
-		self.response.write(template.render())
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/editor.html')
+        self.response.write(template.render())
+
 
 class JsonTestHandler(webapp2.RequestHandler):
-	def get(self):
-		self.response.headers['Content-Type'] = 'application/json'   
-		obj = {
-    			'test': 'success', 
-  		} 
-		self.response.out.write(json.dumps(obj))
+    def get(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        obj = {
+            'test': 'success',
+        }
+        self.response.out.write(json.dumps(obj))
+
 
 application = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/template', TemplatePageHandler),
-	('/editor', EditorPageHandler),
-    ('/test/json', JsonTestHandler)
-], debug=True)
+                                          ('/', MainHandler),
+                                          ('/template', TemplatePageHandler),
+                                          ('/editor', EditorPageHandler),
+                                          ('/test/json', JsonTestHandler)
+                                      ], debug=True)
