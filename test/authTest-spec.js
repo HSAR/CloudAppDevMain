d5d3f5d40694bdd103b1162d7ad3d1779a5f790d
Jonathan Stephens-Jones <jmsj1g12@soton.ac.uk>
2014-11-03 00:16:11 +0000

    [BE] Restored auth html removed in commit da62a9a [Rev: Sam]

--- test/authTest-spec.js
+++ test/authTest-spec.js
@@ -15,25 +15,21 @@ describe('Authentication Tests', function () {
                 // expect to not be on the editor page
                 driver.getCurrentUrl().then(function (currURL) {
                     expect(currURL).toNotBe('http://localhost:8080/editor/');
-                    console.log("FISH 1");
                 });
 
                 // expect to be on the login page
                 expect(loginH3).toBe("Not logged in");
-                    console.log("FISH 2");
 
                 emailElement = driver.findElement(webdriver.By.name("action"));
                 emailElement.click().then(function () {
                     driver.findElement(webdriver.By.css(".auth")).getText().then(function (authedUser) {
                         // expect to be correctly signed in
                         expect(authedUser).toContain("Signed in as test@example.com");
-                    console.log("FISH 3");
                     });
 
                     // expect to have been redirected back onto the editor page
                     driver.getCurrentUrl().then(function (currURL) {
                         expect(currURL).toBe('http://localhost:8080/editor/');
-                        console.log("FISH 4");
                         driver.quit();
                         done();
                     });
