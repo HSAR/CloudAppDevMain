faa08ad9e44f177caaf23cc2a03a1bd66b06b5ef
Murray Colpman <muzer@tim32.org>
2014-11-04 13:17:07 +0000

    Implement test search functionality
    Allow searching some test data. This is just a demo of the basic search
    API for now. To add: advanced search; pagination. [Rev: Nick]

---
+++ src/search.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import os
+import datetime
+import operator
+import copy
+
+import webapp2
+import jinja2
+
+import json
+
+from google.appengine.api import users
+from google.appengine.api import channel
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+test_elements = [ { "name": "UKIP Calypso", "tags": ["racist", "clowns"],
+                    "genre": "Comedy", "author": "The Independents",
+                    "rating": 100, "dateCreated": datetime.date(2014, 10, 20)},
+                  { "name": "Gangnam Style", "tags": ["meme", "irritating"],
+                    "genre": "K-Pop", "author": "The Pumping Lemmas",
+                    "rating": 90, "dateCreated": datetime.date(2012, 07, 15)},
+                  { "name": "Zubion", "tags": ["ou", "est", "la", "gare"],
+                    "genre": "French", "author": "The IT Crowd",
+                    "rating": 20, "dateCreated": datetime.date(1958, 10, 04)} ]
+
+# XXX This is temporary, should use a better method when song class is written
+song_fields = [ "name", "tags", "genre", "author", "rating", "dateCreated" ]
+
+datetimejson = lambda obj: ( obj.isoformat()
+        if isinstance(obj, datetime.date) or isinstance(obj, datetime.datetime)
+        else None )
+
+class SearchPageHandler(webapp2.RequestHandler):
+    def get(self, query=None, sort=None):
+        if not query and not sort:
+            self.abort(400)
+        if sort:
+            if not sort in song_fields:
+                self.abort(400)
+        # This stuff will be handled by the database; this is just temporary to
+        # test the web stuff
+        if query:
+            results = filter(lambda item: query in item["name"]
+                             or query in item["tags"] or query in item["genre"]
+                             or query in item["author"], test_elements)
+        else:
+            results = copy.deepcopy(test_elements)
+        if sort:
+            results.sort(key=operator.itemgetter(sort))
+        self.response.out.write(json.dumps(results, default=datetimejson))
+
+		
+
+		
+application = webapp2.WSGIApplication([
+					webapp2.Route(r'/search', handler=SearchPageHandler, name='search'),
+                                        webapp2.Route(r'/search/<query>', handler=SearchPageHandler, name='search-query'),
+                                        webapp2.Route(r'/search/<query>/<sort>', handler=SearchPageHandler, name='search-query-sort'),
+                                        webapp2.Route(r'/search//<sort>', handler=SearchPageHandler, name='search-sort')
+				      ], debug=True)
