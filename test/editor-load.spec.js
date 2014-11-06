329ed08698ab1a03e2ebbde902ee652c9ae0b018
Mark Mitchell <mjm2g12@soton.ac.uk>
2014-11-06 11:15:35 +0000

    [FE] added jquery import for tests

--- test/editor-load.spec.js
+++ test/editor-load.spec.js
@@ -2,7 +2,7 @@ jQuery = require('jquery');
 $ = jQuery;
 var jsdom = require('jsdom').jsdom;
 
-var jqueryJasmine = require('jasmine-jquery.js');
+var jqueryJasmine = require('./jasmine-jquery.js');
 
 jasmine.getFixtures().fixturesPath = 'my/new/path';
 document = jsdom('<!doctype html><html><head></head><body></body></html>');
