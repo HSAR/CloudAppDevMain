98d65740c126dd9947e00ef0d45463f9208393af
Mark Mitchell <mjm2g12@soton.ac.uk>
2014-11-06 11:53:44 +0000

    [FE]think jasmine-jquery is finally working, running some tests to check

--- test/editor-load.spec.js
+++ test/editor-load.spec.js
@@ -4,10 +4,10 @@ var jsdom = require('jsdom').jsdom;
 
 var jqueryJasmine = require('./jasmine-jquery.js');
 
-jasmine.getFixtures().fixturesPath = 'my/new/path';
-document = jsdom('<!doctype html><html><head></head><body></body></html>');
-window = document.createWindow();
-
+jasmine.getFixtures().fixturesPath = '../src/templates';
+// document = jsdom('<!doctype html><html><head></head><body></body></html>');
+// window = document.createWindow();
+loadFixtures('editor.html');
 
 
 
