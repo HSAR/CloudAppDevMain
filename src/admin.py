6feada859bf281a500e68560ed733e3c9f7bfe8d
Shu Sam Chen <research.hyperspace@gmail.com>
2014-11-11 00:37:27 +0000

    [BE] Added startup method [rev:none]

---
+++ src/admin.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import webapp2
+
+import logging
+
+class Startup(webapp2.RequestHandler):
+    def get(self):
+        logging.info('======== Startup Running ========')
+        logging.info('======== Startup Complete =======')
+
+application = webapp2.WSGIApplication([
+                                          webapp2.Route(r'/_ah/warmup', handler=Startup, name='startup'),
+                                      ], debug=True)
