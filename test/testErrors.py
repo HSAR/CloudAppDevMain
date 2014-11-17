2adca920ff91af7867d4216891fa0ccf36280766
Shu Sam Chen <research.hyperspace@gmail.com>
2014-11-17 17:56:36 +0000

    [BE] Made Nose not freak out when it gets expected errors [rev:Jon]

--- test/testErrors.py
+++ test/testErrors.py
@@ -5,10 +5,10 @@ app = TestApp(application)
 
 # Expect 404 template page as appropriate
 def test_404():
-    response = app.get('/ifthisiseverathingtherewillbetrouble')
+    response = app.get('/ifthisiseverathingtherewillbetrouble', status=404)
     assert str(404) in response.status
 
 # Expect 500 template page as appropriate
 def test_500():
-    response = app.get('/fivehundred')
+    response = app.get('/fivehundred', status=500)
     assert str(500) in response.status
