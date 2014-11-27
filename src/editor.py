1d0f943f1c8cac9b833b3e19f4f7038ae361ccdd
Shu Sam Chen <research.hyperspace@gmail.com>
2014-11-27 16:56:56 +0000

    Initial implementation of providing the editor with song ID [rev:none] Accomplished via URI param, pending further discussion

--- src/editor.py
+++ src/editor.py
@@ -12,19 +12,23 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+
 class EditorPageHandler(webapp2.RequestHandler):
     def get(self):
+        song_id = self.request.get("song")
         user = users.get_current_user()
         if user:
             signout_bar = (
-            'Signed in as %s. (<a href="%s">sign out</a>)' % (user.nickname(), users.create_logout_url('/')))
+                'Signed in as %s. (<a href="%s">sign out</a>)' % (user.nickname(), users.create_logout_url('/')))
             template_values = {
-            'auth_bar': signout_bar
+                'auth_bar': signout_bar,
+                'song_id': song_id
             }
             template = JINJA_ENVIRONMENT.get_template('templates/editor.html')
             self.response.write(template.render(template_values))
         else:
             self.error(401)
+
 
 application = webapp2.WSGIApplication([
                                           webapp2.Route(r'/editor', handler=EditorPageHandler, name='editor'),
