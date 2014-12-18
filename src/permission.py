928d1f1baacff1d215ff02c3d0aa7fc866e3a09d
Jonathan Stephens-Jones <jmsj1g12@soton.ac.uk>
2014-12-18 23:24:21 +0000

    [BE] Updated permissions.py to work with the datastore [rev:Toby]

--- src/permission.py
+++ src/permission.py
@@ -8,6 +8,18 @@ import datastore
 from google.appengine.api import users
 
 def allowed(song_id):
+
+    #user is author, or user is collaborator
+    #user_id = users.get_current_user().user_id()
+    #jigle_list = datastore.getUsersSongs(user_id)
+    #jingle_list.extend(datastore.getUsersCollabs(user_id))
+    #for jingle in jingle_list:
+    #    if jingle.jingle_id == song_id
+    #        return True
+    #return False
+    
     user_id = users.get_current_user().user_id()
-    result = datastore.getCollabInvites(user_id)
-    return song_id in result
+    jingle = datastore.getJingleById(song_id)
+    if jingle and (jingle.author == user_id or user_id in jingle.collab_users):
+        return True
+    return False
