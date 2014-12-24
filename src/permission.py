a02d6c2bc605568f41ebb25515e21eaa16bd3e5f
Toby Finch <tlf1g12@soton.ac.uk>
2014-12-24 15:33:55 +0000

    [BE] Add check that we actually have a user in permissions checks [rev: None]

--- src/permission.py
+++ src/permission.py
@@ -8,16 +8,11 @@ import datastore
 from google.appengine.api import users
 
 def allowed(song_id):
-
-    #user is author, or user is collaborator
-    #user_id = users.get_current_user().user_id()
-    #jigle_list = datastore.getUsersSongs(user_id)
-    #jingle_list.extend(datastore.getUsersCollabs(user_id))
-    #for jingle in jingle_list:
-    #    if jingle.jingle_id == song_id
-    #        return True
-    #return False
     
-    user_id = users.get_current_user().user_id()
-    jingle = datastore.getJingleById(song_id)
-    return jingle and (jingle.author == user_id or user_id in jingle.collab_users)
+    user = users.get_current_user()
+    if user:
+        user_id = user.user_id()
+        jingle = datastore.getJingleById(song_id)
+        return jingle and (jingle.author == user_id or user_id in jingle.collab_users)
+    else:
+        return False
