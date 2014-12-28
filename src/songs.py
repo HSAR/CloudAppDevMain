d1a383600c1118c024619cfd8816d27f30ed6ac4
HSAR <research.hyperspace@gmail.com>
2014-12-28 10:26:11 +0000

    [BE] Render song title into editor page at load [rev:none]

--- src/songs.py
+++ src/songs.py
@@ -336,7 +336,8 @@ class EditorPageHandler(webapp2.RequestHandler):
                 'Signed in as %s. (<a href="%s">sign out</a>)' % (user.nickname(), users.create_logout_url('/')))
             template_values = {
                 'auth_bar': signout_bar,
-                'song_id': songid
+                'song_id': songid,
+                'song_title': datastore.getJingleById(songid).title
             }
             template = JINJA_ENVIRONMENT.get_template('templates/editor.html')
             self.response.write(template.render(template_values))
