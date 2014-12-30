3f818aaeea1ff8cc9d5723798316df504b28f402
Stevie Burrett <sjb2g12@soton.ac.uk>
2014-12-30 14:12:33 +0000

    [FE] Code duplication in table population
    - Population of tables in the dashboard now uses a shared function
    - Link added in anticipation of profile support
    - Leftover CSS removed

--- src/public/js/dash/dashHandler.js
+++ src/public/js/dash/dashHandler.js
@@ -6,38 +6,38 @@ $( document ).ready(function() {
     ajax.getUserInvites(invitedSongs);
 });
 
-//Yeah, there's a lot of code repetition here, I'll get round to fixing that
+
 var ownedSongs = function(response) {
-    var data = jQuery.parseJSON(response);
-    if (!data) {
-        $('#ownedTable').append('<tr><td>No songs found. Why not create one?</td><td></td><td></td><td></td></tr>');
-    } 
-    else {
-        for (var i = 0; i < data.length; i++) {
-            //the parameter for unique ids might change
-            $('#ownedTable').append('<tr><td> <a href="http://jinglr-music.appspot.com/editor/' + data[i].id + '">' + data[i].title + '</a></td><td>' + data[i].owner + "</td><td>" + data[i].tags + "</td><td>" + data[i].genre + "</td></tr>");
-        }
-    }
+    writeToTable(songTables.owned, response);
 }
 
 var collabSongs = function(response) {
+    writeToTable(songTables.collab, response);
+}
+
+var invitedSongs = function(response) {
+    writeToTable(songTables.invites, response);
+}
+
+var writeToTable = function(table, response) {
     var data = jQuery.parseJSON(response);
     if (!data) {
-        $('#collabTable').append('<tr><td>No songs found. Start contributing!</td><td></td><td></td><td></td></tr>');
+        $(table).append('<tr><td>'+ songTableEmptyMessage[table] +'</td><td></td><td></td><td></td></tr>');
     } else {
         for (var i = 0; i < data.length; i++) {
-            $('#collabTable').append('<tr><td> <a href="http://jinglr-music.appspot.com/editor/' + data[i].id + '">' + data[i].title + '</a></td><td>' + data[i].owner + "</td><td>" + data[i].tags + "</td><td>" + data[i].genre + "</td></tr>");
+            $(table).append('<tr><td> <a href="http://jinglr-music.appspot.com/editor/' + data[i].id + '">' + data[i].title + '</a></td><td>' + data[i].owner + "</td><td>" + data[i].tags + "</td><td>" + data[i].genre + "</td></tr>");
         }
     }
 }
 
-var invitedSongs = function(response) {
-    var data = jQuery.parseJSON(response);
-    if (!data) {
-        $('#inviteTable').append('<tr><td>No invitiations.</td><td></td><td></td><td></td></tr>');
-    } else {
-        for (var i = 0; i < data.length; i++) {
-            $('#inviteTable').append('<tr><td> <a href="http://jinglr-music.appspot.com/editor/' + data[i].id + '">' + data[i].title + '</a></td><td>' + data[i].owner + "</td><td>" + data[i].tags + "</td><td>" + data[i].genre + "</td></tr>");
-        }
-    }
+var songTables = {
+    owned: '#ownedTable',
+    collab: '#collabTable',
+    invites: '#inviteTable'
+}
+
+var songTableEmptyMessage = {
+    '#ownedTable': 'No songs found. Why not <a href="#" data-toggle="modal" data-target="#createJingleModal">create one?</a>',
+    '#collabTable': 'No collaborations found. Start contributing!',
+    '#inviteTable': 'No pending invitations'
 }
