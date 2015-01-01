9b6eebcaa3a77466df6ee24724a449e4b91b937f
HSAR <research.hyperspace@gmail.com>
2015-01-01 23:53:15 +0000

    [FE] Fix outdated path to user JSON data. [rev:none]

--- src/public/js/profile/ajaxHandler.js
+++ src/public/js/profile/ajaxHandler.js
@@ -1,15 +1,15 @@
 function AjaxHandler() {
 	this.getUser = function(path, cb) {
-		commonAjax('http://jinglr-music.appspot.com/api/users/' + path + '/json', cb);
-	}
+		commonAjax('http://jinglr-music.appspot.com/api/users/' + path, cb);
+	};
 
 	this.getUserSongs = function(path, cb) {
 		commonAjax('http://jinglr-music.appspot.com/api/users/' + path + '/songs', cb);
-	}
+	};
 	
 	this.getUserCollabs = function(path, cb) {
 		commonAjax('http://jinglr-music.appspot.com/api/users/' + path + '/collabs', cb);
-	}
+	};
 
 	this.updateProfile = function(uid, username, bio, tags, cb) {
 		$.ajax({
