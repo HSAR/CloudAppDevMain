fd4ecc63fa99126a1ed4694697526430f29c4373
Murray Colpman <muzer@tim32.org>
2015-01-02 20:29:38 +0000

    [TESTS] Fix auth tests
    They were using the old URL scheme.

--- test/authTest-spec.js
+++ test/authTest-spec.js
@@ -8,13 +8,13 @@ describe('Authentication Tests', function () {
 
     it('should serve the login page when accessing the editor and without being logged in', function (done) {
 
-        driver.get('http://localhost:8080/songs/0/editor').then(function () {
+        driver.get('http://localhost:8080/web/songs/0').then(function () {
 
             driver.findElement(webdriver.By.tagName("h3")).getText().then(function (loginH3) {
 
                 // expect to not be on the editor page
                 driver.getCurrentUrl().then(function (currURL) {
-                    expect(currURL).toNotBe('http://localhost:8080/songs/0/editor');
+                    expect(currURL).toNotBe('http://localhost:8080/web/songs/0');
                 });
 
                 // expect to be on the login page
@@ -29,7 +29,7 @@ describe('Authentication Tests', function () {
 
                     // expect to have been redirected back onto the editor page
                     driver.getCurrentUrl().then(function (currURL) {
-                        expect(currURL).toBe('http://localhost:8080/songs/0/editor');
+                        expect(currURL).toBe('http://localhost:8080/web/songs/0');
                         driver.quit();
                         done();
                     });
