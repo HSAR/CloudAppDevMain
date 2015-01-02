9a5f8cab8846a095c80e4af4e9b22d199dced009
Murray Colpman <muzer@tim32.org>
2015-01-02 20:40:59 +0000

    [TESTS] Remove "Hello World" test
    Our application no longer says "Hello World", which is rather rude of
    it, but remove the test for this since it's not really something we want
    to test for.

--- test/test.py
+++
@@ -1,9 +0,0 @@
-from webtest import TestApp
-from main import application
-
-app = TestApp(application)
-
-# This is a test of the hello world default application
-def test_index():
-    response = app.get('/')
-    assert 'Hello world!' in str(response)
