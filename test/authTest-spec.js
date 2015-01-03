61367bc17743bbe10d2ba2fb514b9fd9ec8b58b9
Murray Colpman <muzer@tim32.org>
2015-01-03 02:47:03 +0000

    [TESTS] Ensure data is created synchronously

--- test/authTest-spec.js
+++ test/authTest-spec.js
@@ -1,4 +1,4 @@
-require('./createData.js');
+var createData = require('./createData.js');
 var webdriver = require('selenium-webdriver');
 
 var driver = new webdriver.Builder().
@@ -8,31 +8,32 @@ var driver = new webdriver.Builder().
 describe('Authentication Tests', function () {
 
     it('should serve the login page when accessing the editor and without being logged in', function (done) {
-
-        driver.get('http://localhost:8080/web/songs/0').then(function () {
-
-            driver.findElement(webdriver.By.tagName("h3")).getText().then(function (loginH3) {
-
-                // expect to not be on the editor page
-                driver.getCurrentUrl().then(function (currURL) {
-                    expect(currURL).toNotBe('http://localhost:8080/web/songs/0');
-                });
-
-                // expect to be on the login page
-                expect(loginH3).toBe("Not logged in");
-
-                emailElement = driver.findElement(webdriver.By.name("action"));
-                emailElement.click().then(function () {
-                    driver.findElement(webdriver.By.css(".auth")).getText().then(function (authedUser) {
-                        // expect to be correctly signed in
-                        expect(authedUser).toContain("Signed in as test@example.com");
-                    });
-
-                    // expect to have been redirected back onto the editor page
+        createData.createData().then(function () {
+            driver.get('http://localhost:8080/web/songs/0').then(function () {
+    
+                driver.findElement(webdriver.By.tagName("h3")).getText().then(function (loginH3) {
+    
+                    // expect to not be on the editor page
                     driver.getCurrentUrl().then(function (currURL) {
-                        expect(currURL).toBe('http://localhost:8080/web/songs/0');
-                        driver.quit();
-                        done();
+                        expect(currURL).toNotBe('http://localhost:8080/web/songs/0');
+                    });
+    
+                    // expect to be on the login page
+                    expect(loginH3).toBe("Not logged in");
+    
+                    emailElement = driver.findElement(webdriver.By.name("action"));
+                    emailElement.click().then(function () {
+                        driver.findElement(webdriver.By.css(".auth")).getText().then(function (authedUser) {
+                            // expect to be correctly signed in
+                            expect(authedUser).toContain("Signed in as test@example.com");
+                        });
+    
+                        // expect to have been redirected back onto the editor page
+                        driver.getCurrentUrl().then(function (currURL) {
+                            expect(currURL).toBe('http://localhost:8080/web/songs/0');
+                            driver.quit();
+                            done();
+                        });
                     });
                 });
             });
--- test/createData.js
+++ test/createData.js
@@ -4,11 +4,14 @@ var driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.firefox()).
     build();
 
-driver.get('http://localhost:8080/admin/createinitialdata').then(function () {
-  emailElement = driver.findElement(webdriver.By.name("action"));
-  checkElement = driver.findElement(webdriver.By.name("admin"));
-  checkElement.click().then(function () {
-    emailElement.click().then(function () {
+var createData = function() {
+  driver.get('http://localhost:8080/admin/createinitialdata').then(function () {
+    emailElement = driver.findElement(webdriver.By.name("action"));
+    checkElement = driver.findElement(webdriver.By.name("admin"));
+    checkElement.click().then(function () {
+      emailElement.click().then(function () {
+        return driver;
+      });
     });
   });
-});
+};
